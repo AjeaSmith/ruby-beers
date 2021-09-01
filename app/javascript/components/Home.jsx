8a010cf410cb2255e511bb0f1cdd2584f1380241
Ajea Smith <smithajea@rocketmail.com>
2021-09-01 03:50:02 -0400

    created Header/Beers/Home components

--- app/javascript/components/Home.jsx
+++ app/javascript/components/Home.jsx
@@ -1,7 +1,25 @@
 import React from 'react';
+import { Layout } from 'antd';
+import Beers from './Beers';
+import Header from './Header';
 
+const { Content, Footer } = Layout;
 const Home = () => {
-	return <div>Hello world</div>;
+	return (
+		<Layout className="layout">
+			<Header />
+			<Content style={{ padding: '0 50px' }}>
+				<div
+					className="site-layout-content"
+					style={{ margin: '100px auto' }}
+				>
+					<h1>Beer Catalog</h1>
+					<Beers />
+				</div>
+			</Content>
+			<Footer style={{ textAlign: 'center' }}>Honeybadger ©2020.</Footer>
+		</Layout>
+	);
 };
 
 export default Home;
